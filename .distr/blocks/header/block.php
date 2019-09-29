77c2d4316f9731133972d59d22ef5735f6ddcf29
LShirokova <79120151523@ya.ru>
2019-09-29 12:46:35 +0400

    add klogo

--- .distr/blocks/header/block.php
+++ .distr/blocks/header/block.php
@@ -1,7 +1,10 @@
 <header class="header">
 	<div class="container">
 		<nav class="header__nav navbar navbar navbar-expand-lg navbar-light">
-			<a class="navbar-brand" href="#"><img src="img/common/logo.svg" alt=""></a>
+			<a class="navbar-brand" href="#">
+				<img src="img/common/logo.jpg" alt="" style="width: 65px; height: 65px;">
+				<span class="header__name">e-Zвилина</span>
+			</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
